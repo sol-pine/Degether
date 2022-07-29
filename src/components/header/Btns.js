7ec05095ad8d7291b36c65fdd2a18e2cc7e555e8
sol <osunpine@gmail.com>
2022-07-29 20:20:57 +0900

    fix : 채팅 타이밍 에러 해결중

--- src/components/header/Btns.js
+++ src/components/header/Btns.js
@@ -55,43 +55,43 @@ export function LogoutBtn() {
 //   const userId = localStorage.getItem("id");
 //   const [haveNotice, setHaveNotice] = useState(false);
 
-  // useEffect(() => {
-  //   const eventSource = new EventSource(
-  //     `http://degether-back.shop/subscribe/${userId}`
-  //   );
-  // sse 연결
-  // eventSource.onopen = function () {
-  //   axios
-  //     .get(`${SERVER_URL}/api/readsse`, {
-  //       headers: {
-  //         Authorization: localStorage.getItem("token"),
-  //       },
-  //     })
+// useEffect(() => {
+//   const eventSource = new EventSource(
+//     `http://degether-back.shop/subscribe/${userId}`
+//   );
+// sse 연결
+// eventSource.onopen = function () {
+//   axios
+//     .get(`${SERVER_URL}/api/readsse`, {
+//       headers: {
+//         Authorization: localStorage.getItem("token"),
+//       },
+//     })
 
-  //     .then((response) => setHaveNotice(response.data.result));
-  // };
-  // sse 응답
-  // eventSource.addEventListener("sse", function (e) {
-  //   console.log(e);
-  // });
-  // sse 에러
-  //   eventSource.onerror = function (error) {
-  //     console.error(error.message);
-  //   };
-  // }, []);
+//     .then((response) => setHaveNotice(response.data.result));
+// };
+// sse 응답
+// eventSource.addEventListener("sse", function (e) {
+//   console.log(e);
+// });
+// sse 에러
+//   eventSource.onerror = function (error) {
+//     console.error(error.message);
+//   };
+// }, []);
 
-  return (
-    <>
-      <BellButton>
-        {haveNotice ? (
-          <img src="/img/bell-on-icon.svg" alt="bell icon" />
-        ) : (
-          <img src="/img/bell-icon.svg" alt="bell icon" />
-        )}
-      </BellButton>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <BellButton>
+//         {haveNotice ? (
+//           <img src="/img/bell-on-icon.svg" alt="bell icon" />
+//         ) : (
+//           <img src="/img/bell-icon.svg" alt="bell icon" />
+//         )}
+//       </BellButton>
+//     </>
+//   );
+// }
 const HomeButton = styled.button`
   background: none;
   border: none;
@@ -121,7 +121,7 @@ const GrayButton = styled.button`
     color: #09120e;
   }
 `;
-const BellButton = styled.button`
-  background: none;
-  border: none;
-`;
+// const BellButton = styled.button`
+//   background: none;
+//   border: none;
+// `;
