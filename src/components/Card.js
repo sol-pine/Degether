e9955cb531c529c4214f99c5217d897a3c2c08b2
sol <osunpine@gmail.com>
2022-08-03 18:50:17 +0900

    feat : card component re-design

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,10 @@ import React from "react";
 const Card = () => {
   return (
     <div className="card-container">
-      <img src="/img/ic-star.svg" alt="찜 아이콘" className="card-star" />
+      <section>
+        <div className="dday">D-3</div>
+        <img src="/img/ic-star.svg" alt="찜 아이콘" className="card-star" />
+      </section>
       <img src="/img/test-image.png" className="card-image"></img>
       <div className="card-text">
         프로젝트 이름 <br />
